a95a8d22f4828e91bedd2407aa7954b57af58a60
Dhileas Heywood <dhileas.heywood@gmail.com>
2020-05-31 23:04:48 +0100

    Global model completed for now.

--- exploratory_analysis_scripts/exploratory_script_2.R
+++ exploratory_analysis_scripts/exploratory_script_2.R
@@ -16,7 +16,7 @@ sales_global <- read_csv("clean_data/sales_global.csv")
 # selecting the important columns, and removing education, sandbox and boardgame, as they only have 1 or two observations - unreliable.
 model_global_data <- sales_global %>% 
   filter(genre != "Education", genre != "Board Game", genre != "Sandbox") %>% 
-  select(-name, -basename, -publisher, -developer, -rank)
+  select(-name, -basename, -publisher, -developer, -rank, -year)
 
 
 model_global_data %>% 
@@ -43,9 +43,6 @@ global_mod1d <- lm(global_sales ~ publisher_ranked, data = model_global_data)
 summary(global_mod1d)
 # r squared is 0.0009536. Oh wells. 
 
-global_mod1e <- lm(global_sales ~ year, data = model_global_data)
-summary(global_mod1e)
-# r squared is 0.001734. I guess we're not going to get a good model out of these.... 
 
 
 
@@ -84,9 +81,7 @@ global_mod1i <- lm(log_global_sales ~ publisher_ranked, data = model_global_data
 summary(global_mod1i)
 # r squared is 0.01055. Oh wells. 
 
-global_mod1j <- lm(log_global_sales ~ year, data = model_global_data)
-summary(global_mod1j)
-# r squared is 0.03846. I guess we're not going to get a good model out of these.... 
+
 
 par(mfrow = c(2,2))
 plot(global_mod1g)
@@ -111,5 +106,83 @@ global_mod2a <- lm(log_global_sales ~ esrb_rating + publisher_ranked, data = mod
 summary(global_mod2a)
 # r squared is 0.135. This is not much better than the previous model. I'll have a look at the others, and might perform an ANOVA test
 # to see if it's worth adding the predictor. 
+
+global_mod2b <- lm(log_global_sales ~ esrb_rating + platform, data = model_global_data)
+summary(global_mod2b)
+# r squared is 0.171. This is a pretty big improvement, however, it's important to note, that up until now, the p values of each predictor
+# have been (roughly half the time) significant above 0.05. In this case, the re is only one p value < 0.05, and only two others that
+# are < 0.1. This isn't great. If this is the best r squared, again, I'll have to perform an ANOVA
+
+global_mod2c <- lm(log_global_sales ~ esrb_rating + genre, data = model_global_data)
+summary(global_mod2c)
+# r squared is 0.167. Again, and improvement, and the p values are much better. 
+
+anova(global_mod1g, global_mod2b)
+# This shows a p value less than 0.01, so we can reject the null hypothesis and say that mod2b is significantly better than mod1g
+
+# I'll now just have a look at the diagnostic plots
+
+par(mfrow = c(2,2))
+plot(global_mod2b)
+
+# residuals vs fitted looks good. The line is a bit wobbly, but there's no upward or downward trend. The normalQQ is a little off at 
+# either end, but not toooooo much to worry about. Happy with that too. 
+# scale_location looks good as well. There's a slight upward trend, but not too much to be concerned about. It wiggles and goes down as
+# the fitted values increase. And the residuals vs leverage has nothing to be concerned about. 
+
+# looks like we're going with mod2b going forward. 
+
+model_global_remaining_resid <- model_global_data %>% 
+  add_residuals(global_mod2b) %>% 
+  select(-c("log_global_sales", "esrb_rating", "global_sales", "platform"))
+
+model_global_remaining_resid %>% 
+  ggpairs(cardinality_threshold = 17)
+# It's a tough call between genre and publisher_ranked. I'll try both. 
+
+global_mod3a <- lm(log_global_sales ~ esrb_rating + platform + genre, data = model_global_data)
+summary(global_mod3a)
+# r squared is 0.2021. And again, the p values look good. That's not a bad increase tbh. 
+
+global_mod3b <- lm(log_global_sales ~ esrb_rating + platform + publisher_ranked, data = model_global_data)
+summary(global_mod3b)
+# r squared is 0.1774, which is a tiny tiny increase from the last one. 
+
+
+global_mod3c <- lm(log_global_sales ~ esrb_rating + platform + year, data = model_global_data)
+summary(global_mod3c)
+
+
+par(mfrow = c(2,2))
+plot(global_mod3a)
+
+# looks like all of the diagnostic plots are good. They're better than the last lot. 
+
+
+model_global_remaining_resid <- model_global_data %>% 
+  add_residuals(global_mod3a) %>% 
+  select(-c("log_global_sales", "esrb_rating", "global_sales", "platform", "genre"))
+
+model_global_remaining_resid %>% 
+  ggpairs(cardinality_threshold = 17)
+
+# There wasn't much point in doing that, it's the only predictor we have left. 
+
+
+global_mod4 <- lm(log_global_sales ~ esrb_rating + platform + genre + publisher_ranked, data = model_global_data)
+summary(global_mod4)
+# r squared is 0.2079, which is barely an increase. I'm going to perform an ANOVA test to confirm whether it's a useless addition or not
+
+
+anova(global_mod3a, global_mod4)
+# The p value for the anova test is 2.2e-16, which is less than 0.01, which means that we can reject the null hypothesis and say that
+# mod4 is significantly better than mod3a. 
+
+
+# At this point the model can predict 20.8% of the variance in the data, which isn't a whole lot. I'll do this again for the regional 
+# datasets, and see if we can come up with something that might be useful. 
+# I also might revisit this when I've performed the ratings analysis, however, I'm not sure whether I'd like to include ratings in a 
+# model, as a game developer isn't going to have ratings for their game before they publish it, so I'm not sure whether that'd be any
+# use to them at all. Maybe for once the game has been developed. 
 
 
